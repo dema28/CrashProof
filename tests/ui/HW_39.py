66b04c74cf63b6a8864bd4f09210425fa9bdc8f3
Denis Novicov <dema28ster@gmail.com>
2025-03-21 13:27:00 +0100

    modify file

--- tests/ui/HW_39.py
+++ tests/ui/HW_39.py
@@ -14,6 +14,10 @@ class SimpleUITest(unittest.TestCase):
         chrome_options = Options()
         # chrome_options.add_argument("--start-maximized")
         chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--user-data-dir=/tmp/chrome-profile")
 
         self.driver = webdriver.Chrome(options=chrome_options)
 
