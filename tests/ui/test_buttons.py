4e15e0a0b4aa84134ac5838f8d493b3297cee9df
Denis Novicov <dema28ster@gmail.com>
2025-03-21 14:30:16 +0100

    modify

--- tests/ui/test_buttons.py
+++ tests/ui/test_buttons.py
@@ -13,6 +13,7 @@ class SimpleUITest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
