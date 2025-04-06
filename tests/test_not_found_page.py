dcc93a7f86ba19836a4e701cc0c11655b200e444
Denis Novicov <dema28ster@gmail.com>
2025-04-06 10:24:44 +0200

    Add test_not_found_page

--- tests/test_not_found_page.py
+++ tests/test_not_found_page.py
@@ -2,18 +2,17 @@ import pytest
 import requests
 import allure
 from selenium.common import NoSuchElementException
-
 from pages.not_found_page import NotFoundPage
 
-@allure.title("Проверка, что несуществующая страница возвращает статус 404")
+@allure.title("Check that non-existent page returns 404 status")
 @allure.severity(allure.severity_level.CRITICAL)
 def test_404_status_code():
     url = "https://notmodultest1.framer.website"
     response = requests.get(url)
-    assert response.status_code == 404, f"Ожидался статус 404, но получен: {response.status_code}"
+    assert response.status_code == 404, f"Expected 404 status, got:  {response.status_code}"
 
-@allure.title("Проверка UI отображения кастомной страницы 404")
-@allure.description("Проверяется наличие заголовка '404 - Stránka nebyla nalezena'")
+@allure.title("UI check for custom 404 page")
+@allure.description("Checking for heading '404 - Stránka nebyla nalezena'")
 @allure.severity(allure.severity_level.CRITICAL)
 def test_404_page_ui(driver):
     url = "https://modultest1.framer.website/nic-tady-neni"
@@ -22,9 +21,9 @@ def test_404_page_ui(driver):
     page = NotFoundPage(driver)
     try:
         heading = page.get_heading_text()
-        assert "404 - Stránka nebyla nalezena" in heading, f"Найден заголовок: {heading}"
+        assert "404 - Stránka nebyla nalezena" in heading, f"Found heading:  {heading}"
     except NoSuchElementException as e:
         allure.attach(driver.get_screenshot_as_png(),
             name="404 - Stránka nebyla nalezena",
             attachment_type=allure.attachment_type.PNG)
-        pytest.fail("Элемент с заголовком 404 не найден", pytrace=True)
+        pytest.fail("404 heading element not found", pytrace=True)
