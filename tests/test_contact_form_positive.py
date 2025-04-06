dce0672c669b177d38943c19635621c2f1ff52ca
Denis Novicov <dema28ster@gmail.com>
2025-04-06 07:52:47 +0200

    refactor test contact pos

--- tests/test_contact_form_positive.py
+++ tests/test_contact_form_positive.py
@@ -1,7 +1,6 @@
 from time import sleep
 import allure
 import pytest
-from selenium.webdriver.common.by import By
 
 from pages.contact_page import ContactPage
 from configs.test_data import VALID_USER
@@ -28,23 +27,5 @@ def test_contact_form_positive(driver):
     page.submit()
 
     with allure.step("Проверка наличия сообщения об успешной отправке"):
-        if not page.is_success_alert_visible():
-            # Вставляем скриншот в Allure
-            allure.attach(driver.get_screenshot_as_png(),
-                          name="no_success_alert",
-                          attachment_type=allure.attachment_type.PNG)
+        assert page.is_success_alert_visible(), "Сообщение об успешной отправке не найдено"
 
-            # Выводим все p.framer-text
-            paragraphs = driver.find_elements(By.CSS_SELECTOR, "p.framer-text")
-            print("🔍 Содержимое всех <p class='framer-text'> элементов:")
-            for i, p in enumerate(paragraphs):
-                print(f"  [{i}] {p.text.strip()}")
-
-            # Сохраняем html (можно потом загрузить через CI)
-            with open("page_source.html", "w", encoding="utf-8") as f:
-                f.write(driver.page_source)
-
-            pytest.fail("Сообщение об успешной отправке не найдено")
-
-        else:
-            print("Успешное сообщение отображается корректно")
