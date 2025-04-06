01278f738df95a41e66bb83267c5d23e249b3dba
Denis Novicov <dema28ster@gmail.com>
2025-04-06 10:23:57 +0200

    Add test_main_page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -2,14 +2,14 @@ import allure
 from pages.main_page import MainPage
 
 
-@allure.title("Smoke: Загрузка главной страницы")
+@allure.title("Smoke: Loading the Main Page")
 def test_smoke_main_page(driver):
     page = MainPage(driver)
 
-    with allure.step("Открываем главную страницу"):
+    with allure.step("Opening the main page"):
         page.open()
 
-    with allure.step("Проверяем, что заголовок содержит 'Modulconstruct - Líšnice'"):
-        assert page.page_title_is_present(), "Заголовок страницы не содержит ожидаемый текст 'Modulconstruct - Líšnice'"
+    with allure.step("Verifying that the title contains 'Modulconstruct - Líšnice'"):
+        assert page.page_title_is_present(), "The page title does not contain the expected text 'Modulconstruct - Líšnice'"
 
 
